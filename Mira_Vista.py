047bb7461573c4a660fe540b5d7e9f91970051ef
Wendy29477 <wendy.wakaba@strathmore.edu>
2025-07-07 09:27:47 +0300

    Update Mira_Vista.py

--- Mira_Vista.py
+++ Mira_Vista.py
@@ -58,7 +58,7 @@ cluster_profiles = {
             "📧 Send regular emails with curated value offerings and lifestyle tips.",
             "🕰️ Offer relaxed shopping hours and personalized in-store assistance."
         ],
-        "bgcolor": "#E8F5E9"
+        "bgcolor": "#0C0C0C"
     },
     1: {
         "name": "Affluent Minimalists",
